5411a5964d63427668bb69888849ffffb825622e
Nisse Knudsen <nisse.knudsen@gmail.com>
2024-09-04 15:44:23 -0700

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import time
+from datetime import datetime
+
+from make87_messages.text.PlainText_pb2 import PlainText
+from make87 import get_topic, PublisherTopic, topic_names
+
+
+def main():
+    topic = get_topic(name=topic_names.HELLO_WORLD_MESSAGE)
+
+    while True:
+        message = PlainText(body=f"Hello, World!")
+        topic.publish(message)
+        print(f"Published: {message}")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
