0051d1e550a54698697d7654f6a88d2780f16072
nisseknudsen <nisse.knudsen@gmail.com>
2024-10-28 12:48:49 -0700

    Updated to latest SDK API

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ import random
 import cv2
 import numpy as np
 import pygame
-from make87 import initialize, get_topic, topic_names
+from make87 import initialize, get_publisher_topic, resolve_topic_name
 from make87_messages.image.compressed.image_jpeg_pb2 import ImageJPEG
 
 
 def main():
     initialize()
-    topic = get_topic(name=topic_names.SCREEN_CAPTURE)
+    topic_name = resolve_topic_name("SCREEN_CAPTURE")
+    topic = get_publisher_topic(name=topic_name, message_type=ImageJPEG)
 
     def encode_to_jpeg(frame: np.ndarray) -> np.ndarray:
         _, encoded_image = cv2.imencode(".jpeg", frame)
